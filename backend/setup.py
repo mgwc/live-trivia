edb13c63e18d55cf600df05dcf9738a61fa6e622
Mike <>
2021-03-05 15:58:24 -0500

    Admin change

--- backend/setup.py
+++ backend/setup.py
@@ -4,7 +4,7 @@ setup(
     name='trivia',
     version='1.0.0',
     packages=find_packages(),
-    include_package_data=True,                      # Tells Python to include files outside of package directories; MANIFEST.in file specifies which (usually includes static files, templates, etc.)
+    include_package_data=True,      # Tells Python to include files outside of package directories; MANIFEST.in file specifies which (usually includes static files, templates, etc.)
     zip_safe=False,
     install_requires=[
         'flask',
