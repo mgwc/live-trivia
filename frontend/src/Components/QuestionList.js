dacd07c1d56f0169e6e9a21d53bad5b767448a20
Mike <>
2021-03-23 23:00:17 -0400

    Minimal initial code for question_list component

---
+++ frontend/src/Components/QuestionList.js
@@ -0,0 +1,25 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+class QuestionList extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+      // Testing if change is tracked
+    )
+  }
+}
+
+export default QuestionList;
