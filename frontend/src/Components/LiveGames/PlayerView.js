10387f6b5e29d9e2c8bd62f5519c2c092b8a8f85
Mike <>
2021-04-27 22:01:14 -0400

    Addresses CORS issues. Client & server communicating via websockets

--- frontend/src/Components/LiveGames/PlayerView.js
+++ frontend/src/Components/LiveGames/PlayerView.js
@@ -1,14 +1,29 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { io } from "socket.io-client"
 import path from "../../req.js"
 
-
-const socket = io(`${path()}/`)
+const socket = io(`${path()}`)
 
 function PlayerView() {
 
+  useEffect(() => {
+  })
+
+  socket.on('answer_ack', data => {
+    console.log("data = " + data)
+  })
+
+  function handleAnswer() {
+    socket.emit('answer', {data: "hello"})
+  }
+
+  socket.on('connect', function() {
+    socket.emit('connection event', {data: 'Client connected'})
+    console.log("Connection event")
+  })
+
   return (
-    <h1>Hello</h1>
+    <button onClick={handleAnswer}>Join game</button>
   )
 }
 
